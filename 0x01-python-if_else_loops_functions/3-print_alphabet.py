67aa83a0fa4f6b9cd7133bb490e1d620af4a9914
devMiiaHxy <dopemiiah@gmail.com>
2024-03-14 01:24:29 +0100

    all files

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,7 +1,4 @@
 for i in range(ord('a'), ord('z') + 1):
-        if chr(i) != 'e' and chr(i) != 'q':
-                    print('{:c}'.format(i), end='')
-                    for i in range(ord('a'), ord('z') + 1):
-                            if chr(i) != 'e' and chr(i) != 'q':
-                                        print('{:c}'.format(i), end='')
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')
 
